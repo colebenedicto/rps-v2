5d5d7852084cca6a835400fb441f1a6f9c0606a8
Cole Benedicto <colleenadb.2@gmail.com>
2022-07-03 11:00:28 +0800

    Update script.js to fix game logic

--- app/js/script.js
+++ app/js/script.js
@@ -1,50 +1,96 @@
-
 const playerTally = document.querySelector('.player-score');
 const computerTally = document.querySelector('.comp-score');
 const buttons = document.querySelectorAll('.buttons .choice');
 const message = document.querySelector('.message');
 const restart = document.querySelector('.restart');
+const rock = document.querySelector('.rock-btn');
+const paper = document.querySelector('.paper-btn');
+const scissors = document.querySelector('.sci-btn');
+const playerIcon = document.querySelector('.player-icon');
+const compIcon = document.querySelector('.comp-icon');
 
-let compScore = 0;
 let playerScore = 0;
+let compScore = 0;
 let para;
 let playerChoice;
+let compChoice;
+let endMessage;
+let roundWinner;
 
-function game() {
-    buttons.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            playerChoice = btn.id;
-            let compChoice = computerPlay();
-            para = playRound(playerChoice, compChoice);
-            playerTally.textContent = `You: ${playerScore}`;
-            computerTally.textContent = `Computer: ${compScore}`;
-            message.textContent = para;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerChoice = button.id;
 
-            if (Math.max(playerScore, compScore) === 5) {
-                let endMessage = document.createElement('p');
-                endMessage.textContent = `Game over! ${playerScore > compScore ? 'You win!' : 'Computer wins!'}`;
-                message.appendChild(endMessage);
-                restart.addEventListener('click', reset());
-            }
-        });
+        compChoice = computerPlay();
+        roundWinner = playRound(playerChoice, compChoice); 
+        updateScore();
+        updateRPSIcons(playerChoice, compChoice);
+
+        if (Math.max(playerScore, compScore) === 5) {
+            endGame();
+        }
     });
+});
+
+function updateScore() {
+  if (roundWinner === 'tie') {
+    message.textContent = "It's a tie!"
+  } else if (roundWinner === 'player') {
+    message.textContent = 'You won!'
+  } else if (roundWinner === 'computer') {
+    message.textContent = 'You lost!'
+  }
+
+  playerTally.textContent = `You: ${playerScore}`;
+  computerTally.textContent = `Computer: ${compScore}`
 }
 
-function reset() {
-    compScore = 0;
-    playerScore = 0;
-    playerTally.textContent = 'You: ';
-    computerTally.textContent = 'Computer: ';
-    message.innerHTML = 'Start';
-    game();
+function updateRPSIcons(player, comp) {
+    switch(player) {
+        case 'rock':
+            playerIcon.src = 'images/rock-player.png';
+            break;
+        case 'paper':
+            playerIcon.src = 'images/paper-player.png';
+            break;
+        case 'scissors':
+            playerIcon.src = 'images/scissors-player.png';
+            break;
+    }
+
+    switch(comp) {
+        case 'rock':
+            compIcon.src = 'images/rock-comp.png';
+            break;
+        case 'paper':
+            compIcon.src = 'images/paper-comp.png';
+            break;
+        case 'scissors':
+            compIcon.src = 'images/scissors-comp.png';
+    }
+}
+
+function buttonsDisabled(isFalse) {
+    buttons.forEach(button => {
+        button.disabled = isFalse;
+    });
 }
 
 restart.addEventListener('click', () => {
     playerTally.textContent = 'You: ';
     computerTally.textContent = 'Computer: ';
     message.innerHTML = 'Start!';
-    game();
-});
+    buttonsDisabled(false);
+}); 
+
+function endGame() {
+    buttonsDisabled(true);
+    endMessage = document.createElement('p');
+    endMessage.textContent = `Game over! ${playerScore > compScore ? 'You win!' : 'Computer wins!'}`;
+    message.textContent = endMessage.textContent;
+    playerScore = 0;
+    compScore = 0;
+}
 
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -57,23 +103,25 @@ function playRound(playerSelection, compSelection) {
         (playerSelection === 'paper' && compSelection === 'rock') ||
         (playerSelection === 'scissors' && compSelection === 'paper')) {
         playerScore++;
-        return 'You win!';
+        return 'player';
             ;
     } else if ((compSelection === 'rock' && playerSelection === 'scissors') ||
         (compSelection === 'paper' && playerSelection === 'rock') ||
         (compSelection === 'scissors' && playerSelection === 'paper')) {
         compScore++;
-        return 'You Lose.';
+        return 'computer';
     } else {
-        return 'It\'s a tie!';
+        return 'tie';
     } 
 }
 
-game();
+// game();
 
 /* 
 Functionalities to add in the next version/update:
- -- startGame() - runs only every time the website is opened or refreshed
- -- endGame() - ends game; disables buttons until 'New Game' button is clicked;
- -- "New Game" modal 
+ updateRoundMessage function that does: _ beats _, _ is beaten by _, _ ties with _.
 */
+
+// eventListener for playerTall, compTally, and message to blink every time they're updated
+
+//  I really just need to know how the addEventListener works.
